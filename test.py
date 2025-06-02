800a5ca4ab509cf2fb4aa964d9ea5794d101386c
jingpeng7527 <jingpeng7527@gmail.com>
2025-06-02 16:16:41 -0700

    kill test after 10mins

--- test.py
+++ test.py
@@ -2,12 +2,13 @@ import subprocess
 import os
 import time
 
-def test(model_name, model_file="My Behavior.onnx" , test_type="Perturbation"):
+def test(model_name, model_file="My Behavior.onnx", test_type="Perturbation", duration=600):
     """
     Args:
         model_name: Name of the model to be tested
         model_file: Path to the ONNX model file
         test_type: Type of test to run (e.g., "Perturbation", "Normal", "Random")
+        duration: Duration of the test in seconds (default: 60)
     """
 
     exe_path = r"C:/Users/BionicVisionVR/Documents/Mouse/2D go to target v1/Builds/" + test_type + "/2D go to target v1.exe"
@@ -20,6 +21,16 @@ def test(model_name, model_file="My Behavior.onnx" , test_type="Perturbation"):
     time.sleep(1)
 
     """
-    call LoadModels in Unity executable
+    call LoadModels in Unity executable and close it after duration
     """
-    subprocess.Popen([exe_path, "--model=" + model_file])
+    process = subprocess.Popen([exe_path, "--model=" + model_file])
+    
+    # Wait for the specified duration
+    time.sleep(duration)
+    
+    # Close the process
+    process.terminate()
+    try:
+        process.wait(timeout=5)  # Wait up to 5 seconds for the process to terminate
+    except subprocess.TimeoutExpired:
+        process.kill()  # Force kill if it doesn't terminate gracefully
